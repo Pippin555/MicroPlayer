eb87b77e0afe3482a07f6578f098c5398b481641
pippin555 <harm.salomons@gmail.com>
2025-06-26 20:09:09 +0200

    compile and requirements reorganized

--- compile.bat
+++ compile.bat
@@ -1,9 +1,8 @@
 rem %1 is the folder of the pyinstaller
 %1/pyinstaller micro_player.py --onefile --noconsole
 :
-rem %1/pyinstaller select.py --onefile --noconsole
-:
 copy dist\micro_player.exe c:\bin\micro_player.exe
-
+:
 rem select.exe might be re-introduced in future
+rem %1/pyinstaller select.py --onefile --noconsole
 rem copy dist\select.exe c:\bin\select.exe
